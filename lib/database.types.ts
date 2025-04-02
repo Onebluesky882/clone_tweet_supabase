27358dc729c774facd172c9dc50034998b5512a9
Onebluesky882 <onebluesky882@outlook.com>
2025-04-02 16:57:27 +0700

    update setup foreign key

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -14,16 +14,19 @@ export type Database = {
           created_at: string
           id: string
           title: string | null
+          user_id: string
         }
         Insert: {
           created_at?: string
           id?: string
           title?: string | null
+          user_id: string
         }
         Update: {
           created_at?: string
           id?: string
           title?: string | null
+          user_id?: string
         }
         Relationships: []
       }
